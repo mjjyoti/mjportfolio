c664613a430a196e7212a2e841ef6d74f966005f
jyoti mahajan <jyotimahajan800@gmail.com>
2024-06-24 14:47:36 +0530

    end code

---
+++ lib/constants/sns_links.dart
@@ -0,0 +1,8 @@
+class SnsLinks{
+  static const String github="https://github.com/mjj";
+  static const String linkedin="https://www.linkedin.com/in/jyoti-mahajan-0818b41b4/";
+  static const String gmail="jyotimahajan800@gmail.com";
+  static const String instagram="https://www.instagram.com/mj_jyoti29/";
+  static const String facebook="https://www.facebook.com/";
+
+}
